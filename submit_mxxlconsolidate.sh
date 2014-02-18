e9344610577672979c1094871f9227b57a8f2ad6
Douglas Applegate <dapple@astro.uni-bonn.de>
2014-02-18 12:47:59 +0100

    first go at parallelizing consolidate

---
+++ submit_mxxlconsolidate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#############
+
+jobdir=$1
+torun=$2
+
+if [ -e $resubdir/consolidate.condor.submit ]; then
+    rm $resubdir/consolidate.condor.submit
+fi
+
+for config in `cat torun`; do
+
+    sed -e "s|JOBDIR|$jobdir|g" -e "s/CONFIG/$config/g" condor.consolidate.template >> $jobdir/consolidate.condor.submit
+
+ 
+done
