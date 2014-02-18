aac1af4aaa571b8808c1b8f67e782ca9f9a0bd7f
Douglas Applegate <dapple@stanford.edu>
2014-02-18 09:46:23 -0800

    bug fix update to lsfconsolidate.sh

--- submit_lsfconsolidate.sh
+++ submit_lsfconsolidate.sh
@@ -6,8 +6,8 @@ torun=$1
 
 
 for config in `cat $torun`; do 
-    bsub -q long -oo fullrun/consolidate.$config.log ./consolidate_fits.py bcc ~/nfs/bcc_clusters/recentered/$config
-    bsub -q long -oo fullrun/consolidate.$config.log ./consolidate_fits.py bk11snap141 ~/nfs/beckersims/snap141/intlength400/$config
-    bsub -q long -oo fullrun/consolidate.$config.log ./consolidate_fits.py bk11snap124 ~/nfs/beckersims/snap124/intlength400/$config
+    bsub -q long -oo fullrun/consolidate.$config.log ./consolidate_fits.py . bcc ~/nfs/bcc_clusters/recentered/$config
+    bsub -q long -oo fullrun/consolidate.$config.log ./consolidate_fits.py . bk11snap141 ~/nfs/beckersims/snap141/intlength400/$config
+    bsub -q long -oo fullrun/consolidate.$config.log ./consolidate_fits.py . bk11snap124 ~/nfs/beckersims/snap124/intlength400/$config
 
 done
