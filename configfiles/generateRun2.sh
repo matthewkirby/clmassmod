b81681d9f47461b0e757cdbcabc8d65eaabddc2a
Douglas Applegate <dapple@astro.uni-bonn.de>
2014-02-21 16:38:08 +0100

    updated some config files

--- configfiles/generateRun2.sh
+++ configfiles/generateRun2.sh
@@ -8,7 +8,7 @@ for i in 0 1 2 3 4; do
 
 	    config=${mc}-hst_${hst}_${i}
 	    mkdir ../mxxl_imperial/snap41/$config
-	    cat base50.sh ${mc}.sh hst_${hst}_${i}.sh > ../mxxl_imperial/snap41/$config/config.sh
+	    cat base50.sh ${mc}.sh r6.sh hst_${hst}_${i}.sh > ../mxxl_imperial/snap41/$config/config.sh
 
 	    echo $config >> ../run2
 
