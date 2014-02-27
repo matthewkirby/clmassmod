7db849530c420e43a843b2e93c68d9a8250b3894
Douglas Applegate <dapple@astro.uni-bonn.de>
2014-02-27 15:19:47 +0100

    draft of expanded binning options

--- basicBinning.py
+++ basicBinning.py
@@ -82,3 +82,50 @@ class bootstrapequalbins(object):
         return np.array(radii), np.array(shear), np.array(shearerr)
             
 
+##############################
+
+class bootstrapfixedbins(object):
+
+    def __init__(self, config = None):
+        self.ngals = 200
+        self.maxradii = 3.
+        self.minradii = 0.
+        self.binspacing = 'linear'
+        self.nbins = 12.
+        self.profileCol = 'r_mpc'
+
+        if config is not None:
+            self.ngals = config.ngals
+            self.maxradii = config.profilemax
+            self.minradii = config.profilemin
+            self.binspacing = config.binspacing
+            self.nbins = config.nbins
+            self.profileCol = config.profilecol
+        
+
+    def __call__(self, catalog, config):
+
+        if self.binspacing == 'linear':
+            binedges = np.linspace(self.minradii, self.maxradii, self.nbins+1)
+        else:
+            binedges = np.logspace(np.log10(self.minradii), np.log10(self.maxradii), self.nbins+1)
+
+        radii = []
+        shear = []
+        shearerr = []
+        for i in range(self.nbins):
+            mintake = binedges[i]
+            maxtake = binedges[i+1]
+            selected = catalog.filter(np.logical_and(catalog[self.profileCol] >= mintake,
+                                                     catalog[self.profileCol] < maxtake))
+
+            
+
+            radii.append(np.mean(selected['r_mpc']))
+
+            curmean, curerr = bootstrapmean(selected['ghat'])
+            shear.append(curmean)
+            shearerr.append(curerr)
+
+        return np.array(radii), np.array(shear), np.array(shearerr)
+      
