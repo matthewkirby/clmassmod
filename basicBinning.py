9f4c2c2541c08b4a05ff8228c463c5d200393ffe
Douglas Applegate <dapple@astro.uni-bonn.de>
2014-02-27 15:04:23 +0100

    added bootstrapped uncertainties for binning

--- basicBinning.py
+++ basicBinning.py
@@ -34,5 +34,51 @@ class dumbequalbins(object):
             shearerr.append(np.std(sorted_cat['ghat'][i:maxtake])/np.sqrt(maxtake-i))
 
         return np.array(radii), np.array(shear), np.array(shearerr)
+
+############################
+
+def bootstrapmean(distro, nboot=1000):
+
+    bootedmeans = np.zeros(nboot)
+    for i in range(nboot):
+        curboot = np.random.randint(0, len(distro), len(distro))
+        bootedmeans[i] = np.mean(distro[curboot])
+
+    return np.mean(bootedmeans), np.std(bootedmeans)
+
+############################
+
+class bootstrapequalbins(object):
+
+    def __init__(self, config = None):
+        self.ngals = 200
+        self.maxradii = 2000
+        self.minradii = 0
+        self.profileCol = 'r_mpc'
+
+        if config is not None:
+            self.ngals = config.ngals
+            self.maxradii = config.profilemax
+            self.minradii = config.profilemin
+            self.profileCol = config.profilecol
+        
+
+    def __call__(self, catalog, config):
+
+        sorted_cat = catalog.filter(np.argsort(catalog[self.profileCol]))
+        sorted_cat = sorted_cat.filter(np.logical_and(sorted_cat[self.profileCol] > self.minradii, 
+                                                      sorted_cat[self.profileCol] < self.maxradii))
+        radii = []
+        shear = []
+        shearerr = []
+        for i in range(0, len(sorted_cat), self.ngals):
+            maxtake = min(i+self.ngals, len(catalog))
+            radii.append(np.mean(sorted_cat['r_mpc'][i:maxtake]))
+
+            curmean, curerr = bootstrapmean(sorted_cat['ghat'][i:maxtake])
+            shear.append(curmean)
+            shearerr.append(curerr)
+
+        return np.array(radii), np.array(shear), np.array(shearerr)
             
 
