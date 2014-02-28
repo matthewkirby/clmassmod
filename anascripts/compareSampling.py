f84b12cc3cd6d8617c41191a1f44595a07aec8f6
Douglas Applegate <dapple@astro.uni-bonn.de>
2014-02-28 17:27:28 +0100

    trying to catch up on notebook, added noise bias plots

--- anascripts/compareSampling.py
+++ anascripts/compareSampling.py
@@ -12,41 +12,41 @@ bccdir='/users/dapple/braid1/bcc'
 
 mcrs = 'c4 cfree'.split()
 radialranges = ['r{0}'.format(i) for i in '1 5 10'.split()]
-#samplings = ['n0_0'] + ['n{0}_2'.format(i) for i in range(6)]
-samplings = ['n{0}_0'.format(i) for i in range(6)]
+samplings = ['n0_0'] + ['n{0}_2'.format(i) for i in range(6)]
+#samplings = ['n{0}_0'.format(i) for i in range(6)]
 
 
 #mxxldata = {}
 #bccdata = {}
-##
-#for mcr in mcrs:
 #
-#    if mcr not in mxxldata:
-#        mxxldata[mcr] = {}
-#    if mcr not in bccdata:
-#        bccdata[mcr] = {}
-#
-#    for radrange in radialranges:
-#
-#        if radrange not in mxxldata[mcr]:
-#            mxxldata[mcr][radrange] = {}
-#        if radrange not in bccdata[mcr]:
-#            bccdata[mcr][radrange] = {}
-#
-#        for sample in samplings:
-#
-#            simname = '{0}-{1}-{2}'.format(mcr, radrange, sample)
-#            mxxlfilename='{0}/{1}/consolidated.pkl'.format(mxxldir, simname)
-#
-#            mxxldata[mcr][radrange][sample] = bbb.loadData(mxxlfilename)
-#
-#            bccfilename='{0}/{1}/consolidated.pkl'.format(bccdir, simname)
-#            if not os.path.exists(bccfilename):
-#                print 'Skipping BCC {0}'.format(simname)
-#                continue
-#            bccdata[mcr][radrange][sample] = bbb.loadData(bccfilename)
-#   
-#
+for mcr in mcrs:
+
+    if mcr not in mxxldata:
+        mxxldata[mcr] = {}
+    if mcr not in bccdata:
+        bccdata[mcr] = {}
+
+    for radrange in radialranges:
+
+        if radrange not in mxxldata[mcr]:
+            mxxldata[mcr][radrange] = {}
+        if radrange not in bccdata[mcr]:
+            bccdata[mcr][radrange] = {}
+
+        for sample in samplings:
+
+            simname = '{0}-{1}-{2}'.format(mcr, radrange, sample)
+            mxxlfilename='{0}/{1}/consolidated.pkl'.format(mxxldir, simname)
+
+            mxxldata[mcr][radrange][sample] = bbb.loadData(mxxlfilename)
+
+            bccfilename='{0}/{1}/consolidated.pkl'.format(bccdir, simname)
+            if not os.path.exists(bccfilename):
+                print 'Skipping BCC {0}'.format(simname)
+                continue
+            bccdata[mcr][radrange][sample] = bbb.loadData(bccfilename)
+   
+
 
 
 c = 'SlateGray r b m DodgerBlue g DarkSalmon'.split()
@@ -100,101 +100,102 @@ sampling_trans = {
 'n5_2' : '1 0.33',
 
 }
+
+
+figsize(8,6)
+
+####
+# Bias
+
+for mcr in mcrs:
+
+    for radrange in radialranges:
+
+        figure()
+
+
+        ccount = 0        
+        for curdens, sampling in enumerate(samplings):
+
+            axhline(1.0, c='DimGray', linestyle='--')
+
+#            linecount = 0
+#            if sampling in bccdata[mcr][radrange]:
+#                simdat = bccdata[mcr][radrange][sampling]
+#                massbin, ratiobin, ratioerr = bbb.summary2DMass(simdat)
+#                plot(massbin, ratiobin, color=c[ccount], linestyle=linestyle[linecount], 
+#                     label='BCC {0}'.format(sampling_trans[sampling]))
 #
-#
-#figsize(8,8)
-#
-#####
-## Bias
-#
-#for mcr in mcrs:
-#
-#    for radrange in radialranges:
-#
-#        figure()
-#
-#
-#        ccount = 0        
-#        for curdens, sampling in enumerate(samplings):
-#
-#            axhline(1.0, c='DimGray', linestyle='--')
-#
-##            linecount = 0
-##            if sampling in bccdata[mcr][radrange]:
-##                simdat = bccdata[mcr][radrange][sampling]
-##                massbin, ratiobin, ratioerr = bbb.summary2DMass(simdat)
-##                plot(massbin, ratiobin, color=c[ccount], linestyle=linestyle[linecount], 
-##                     label='BCC {0}'.format(sampling_trans[sampling]))
-##
-#
-#
-#            linecount = 1
-#            simdat = mxxldata[mcr][radrange][sampling]
-#            massbin, ratiobin, ratioerr = bbb.summary2DMass(simdat)
-#            plot(massbin, ratiobin, color=c[ccount], linestyle=linestyle[linecount], 
-#                 label='MXXL {0}'.format(sampling_trans[sampling]))
-#
-#            ccount += 1
-#
-#
-#
-#
-#
-#
-#
-#
-#        xlabel(r'Mass $M_{200}$', fontsize=16)
-#        ylabel(r'Ratio $M_{\mathrm{lensing}} / M_{\mathrm{true}}$', fontsize=16)
-#        axis([14.1, 15.2, 0.7, 1.3])
-#        legend(loc='upper left')
-#        title('MC Relation: {0} RadialRange: {1}'.format(mcr, radrange_trans[radrange]))
-#        tight_layout()
-#        savefig('density_noise2_{0}-{1}.png'.format(mcr, radrange))
-#
+
+
+            linecount = 1
+            simdat = mxxldata[mcr][radrange][sampling]
+            massbin, ratiobin, ratioerr = bbb.summary2DMass(simdat)
+            plot(massbin, ratiobin, color=c[ccount], linestyle=linestyle[linecount], 
+                 label='MXXL {0}'.format(sampling_trans[sampling]))
+
+            ccount += 1
+
+
+
+
+
+
+
+
+        xlabel(r'Mass $M_{200}$', fontsize=16)
+        ylabel(r'Ratio $M_{\mathrm{lensing}} / M_{\mathrm{true}}$', fontsize=16)
+        axis([14.1, 15.2, 0.7, 1.3])
+        legend(loc='upper left')
+        title('MC Relation: {0} RadialRange: {1}'.format(mcr, radrange_trans[radrange]))
+        tight_layout()
+        savefig('density_noise2_{0}-{1}.png'.format(mcr, radrange))
+
 ####
 ## Scatter
 
 
-#for mcr in mcrs:
-#
-#    for radrange in radialranges:
-#
-#        figure()
-#
-#
-#        ccount = 0        
-#        for curdens, sampling in enumerate(samplings):
-#
-#            axhline(1.0, c='DimGray', linestyle='--')
-#
-#            linecount = 0
-#            if sampling in bccdata[mcr][radrange]:
-#                simdat = bccdata[mcr][radrange][sampling]
-#                massbin, massbin_min, massbin_max, intrinsic_scatter, median_err, sym_distro, logratiodists, sigmadists = bbb.scatterSummary(simdat)
-#                plot(massbin, sym_distro, color=c[ccount], linestyle=linestyle[linecount],
-#                     label='BCC {0}'.format(sampling_trans[sampling]))
-#
-#
-#
-#            linecount = 1
-#            simdat = mxxldata[mcr][radrange][sampling]
-#            massbin, massbin_min, massbin_max, intrinsic_scatter, median_err, sym_distro, logratiodists, sigmadists = bbb.scatterSummary(simdat)
-#            plot(massbin, sym_distro, color=c[ccount], linestyle=linestyle[linecount],
-#                 label='MXXL {0}'.format(sampling_trans[sampling]))
-#
-#            ccount += 1
-#
-#
-#    
-#        xlabel(r'Mass $M_{200}$', fontsize=16)
-#        ylabel(r'Distribution Scatter', fontsize=16)
-#        axis([14.1, 15., 0.0, 0.6])
-#        legend(loc='upper left')
-#        title('Scatter - MC Relation: {0} Radial Range: {1}'.format(mcr, radrange_trans[radrange]))
-#        tight_layout()
+for mcr in mcrs:
+
+    for radrange in radialranges:
+
+        figure()
+
+
+        ccount = 0        
+        for curdens, sampling in enumerate(samplings):
+
+            axhline(1.0, c='DimGray', linestyle='--')
+
+            linecount = 0
+            if sampling in bccdata[mcr][radrange]:
+                simdat = bccdata[mcr][radrange][sampling]
+                massbin, massbin_min, massbin_max, intrinsic_scatter, median_err, sym_distro, logratiodists, sigmadists = bbb.scatterSummary(simdat)
+                plot(massbin, sym_distro, color=c[ccount], linestyle=linestyle[linecount],
+                     label='BCC {0}'.format(sampling_trans[sampling]))
+
+
+
+            linecount = 1
+            simdat = mxxldata[mcr][radrange][sampling]
+            massbin, massbin_min, massbin_max, intrinsic_scatter, median_err, sym_distro, logratiodists, sigmadists = bbb.scatterSummary(simdat)
+            plot(massbin, sym_distro, color=c[ccount], linestyle=linestyle[linecount],
+                 label='MXXL {0}'.format(sampling_trans[sampling]))
+
+            ccount += 1
+
+
+    
+        xlabel(r'Mass $M_{200}$', fontsize=16)
+        ylabel(r'Distribution Scatter', fontsize=16)
+        axis([14.1, 15., 0.0, 0.6])
+        legend(loc='upper left')
+        title('Scatter - MC Relation: {0} Radial Range: {1}'.format(mcr, radrange_trans[radrange]))
+        tight_layout()
+        savefig('density_noise2_{0}-{1}_scatter.png'.format(mcr, radrange))
 #        savefig('density_{0}-{1}_scatter.png'.format(mcr, radrange))
-#
-#
+
+
 
 ################
 # Investigation of BCC Scatter
@@ -202,7 +203,7 @@ sampling_trans = {
 #
 #
 #
-simdat = bccdata['cfree']['r10']['n0_0']
+#simdat = bccdata['cfree']['r10']['n0_0']
 #
 ##bias
 #figure()
@@ -232,35 +233,36 @@ simdat = bccdata['cfree']['r10']['n0_0']
 
 ####scatter
 #
-figure()
-ccount = 0
-linecount=1
-zbins = [(0., 0.3), (0.35, 0.5), (0.5, 0.89), (0.9, 1.5)]
-
-for zbin in zbins:
-
-    massbin, massbin_min, massbin_max, intrinsic_scatter, median_err, sym_distro, logratiodists, sigmadists = bbb.scatterSummary(simdat, np.logical_and(simdat['redshifts'] >= zbin[0],simdat['redshifts'] < zbin[1]))
-    plot(massbin, sym_distro, color=c[ccount], linestyle=linestyle[linecount],
-         label='{0} {1}'.format(*zbin))
-
-    ccount += 1
-
-
-xlabel(r'Mass $M_{200}$', fontsize=16)
-ylabel(r'Distribution Scatter', fontsize=16)
-axis([14.1, 15., 0.0, 0.6])
-legend(loc='upper left')
-title('BCC Scatter Redshift Evolution')
-tight_layout()
-savefig('bcc_scatter_redshift_evo_r10_n0_0.png')
-
+#figure()
+#ccount = 0
+#linecount=1
+#zbins = [(0., 0.3), (0.35, 0.5), (0.5, 0.89), (0.9, 1.5)]
+#
+#for zbin in zbins:
+#
+#    massbin, massbin_min, massbin_max, intrinsic_scatter, median_err, sym_distro, logratiodists, sigmadists = bbb.scatterSummary(simdat, np.logical_and(simdat['redshifts'] >= zbin[0],simdat['redshifts'] < zbin[1]))
+#    plot(massbin, sym_distro, color=c[ccount], linestyle=linestyle[linecount],
+#         label='{0} {1}'.format(*zbin))
+#
+#    ccount += 1
+#
+#
+#xlabel(r'Mass $M_{200}$', fontsize=16)
+#ylabel(r'Distribution Scatter', fontsize=16)
+#axis([14.1, 15., 0.0, 0.6])
+#legend(loc='upper left')
+#title('BCC Scatter Redshift Evolution')
+#tight_layout()
+#savefig('bcc_scatter_redshift_evo_r10_n0_0.png')
+#
 
 
 
 ############
 # Evolution with noise at fixed sampling
 
-noisesamplings = ['n2_{0}'.format(i) for i in range(4)]
+sampledens = 3
+noisesamplings = ['n{0}_{1}'.format(sampledens, i) for i in range(4)]
 
 ###load data
 #for mcr in mcrs:
@@ -291,8 +293,8 @@ noisesamplings = ['n2_{0}'.format(i) for i in range(4)]
 #            bccdata[mcr][radrange][sample] = bbb.loadData(bccfilename)
 #
 #
-#
-figsize(8,8)
+
+#figsize(8,6)
 
 ####
 # Bias
@@ -339,12 +341,12 @@ figsize(8,8)
 #        legend(loc='upper left')
 #        title('MC Relation: {0} RadialRange: {1}'.format(mcr, radrange_trans[radrange]))
 #        tight_layout()
-##        savefig('noise_{0}-{1}.png'.format(mcr, radrange))
+#        savefig('noise_{0}-{1}-{2}.png'.format(mcr, radrange, sampling))
 #
 ####
-## Scatter
-#
-#
+# Scatter
+
+
 #for mcr in mcrs:
 #
 #    for radrange in radialranges:
@@ -370,7 +372,7 @@ figsize(8,8)
 #            simdat = mxxldata[mcr][radrange][sampling]
 #            massbin, massbin_min, massbin_max, intrinsic_scatter, median_err, sym_distro, logratiodists, sigmadists = bbb.scatterSummary(simdat)
 #            plot(massbin, sym_distro, color=c[ccount], linestyle=linestyle[linecount],
-#                 label='BCC {0}'.format(sampling_trans[sampling]))
+#                 label='MXXL {0}'.format(sampling_trans[sampling]))
 #
 #            ccount += 1
 #
@@ -382,9 +384,9 @@ figsize(8,8)
 #        legend(loc='upper left')
 #        title('Scatter - MC Relation: {0} Radial Range: {1}'.format(mcr, radrange_trans[radrange]))
 #        tight_layout()
-#        savefig('noise_{0}-{1}_scatter.png'.format(mcr, radrange))
-
-
+#        savefig('noise_{0}-{1}-{2}_scatter.png'.format(mcr, radrange, sampling))
+#
+#
 
 ########################
 
