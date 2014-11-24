4d847bcda7c692eb03bb5e0a4d11120b1b1c70fb
Douglas Applegate <douglas.e.applegate@gmail.com>
2014-11-24 10:08:47 +0100

    bus edits

--- measurebias.py
+++ measurebias.py
@@ -18,55 +18,37 @@ def loadClusterData(simdir, chaindir):
 
 ####################
 
+def massbinModel(parts, clusters, massbinedges = np.logpsace(np.log10(1e14), np.log10(5e15), 10)):
+    #constant spaced log mass bins
 
-def buildModel(clusters):
-    #buid pymc model that infers bias, scatter, and m-c model
+    nbins = len(massbinedges) - 1
 
-    parts = {}
+    parts['logmassbiasratios'] = np.empty(nbins, dtype=object)
+    parts['c200s'] = np.empty(nbins, dtype=object)
+    for i in range(nbins):
+        parts['logmassratios'][i] = pymc.Uninformative('logmassbias_%d' % i)
+        parts['c200s'][i]= pymc.Uniform('c200_%d' % i, 1.1, 19.9)
 
-    #start with a linear model for \vec alpha, relating log(m_true) and log(m_lens)
+    parts['binnedclusters'] = []
+    for i in range(nbins):
+        curbin = {}
+        ### SET UP DATA IN CORRECT ORDERING HERE
+        parts['binnedclusters'].append(curbin)
 
-    parts['massnorm'] = 5e14
-    parts['log_massnorm'] = np.log(massnorm)
-
-    parts['theta'] = pymc.Uniform('theta', -np.pi/2., np.pi/2.)
-    
-    @pymc.deterministic
-    def slope(theta = parts['theta']):
-        return np.tan(theta)
-    parts['slope'] = slope
-
-    parts['offset'] = pymc.Uniform('offset', -100., 100.)
-
-    parts['log_mtrue_norm'] = np.array([cluster['log_mtrue'] for cluster in clusters]) - parts['log_massnorm']
-
-    @pymc.deterministic(trace = False)
-    def log_mlensing_pred(x=parts['log_mtrue_norm'], m= parts['slope'], b = parts['offset']):
-        return m*x +b
-    parts['pred_log_mlens_norm'] = model
-    
-    parts['pred_c200'] = 4.*np.ones(len(clusters))
-
-    parts['logmlens_log_scatter'] = pymc.Uniform('logmlens_log_scatter', np.log(1e-4), np.log(1.))
-    @pymc.deterministic
-    def logmlens_scatter(logscatter = parts['logmlens_log_scatter']):
-        return np.exp(logscatter)
-    parts['logmlens_scatter'] = logmlens_scatter
-
-    parts['log_c200_scatter'] = 0.116*np.log(10)
+        
+#####################
 
 
-    parts['like_log_mlens_norm'] = [cluster['log_mlens'] - parts['log_massnorm'] for cluster in clusters]
-    parts['like_c200'] = [cluster['c200'] for cluster in clusters]
-    #no weights are needed since I'll compute P(log_mass) and the chain used P(log_mass) = const
+
+def buildLikelihood(parts):
+
 
     
-
-    
-    
-
 
     @pymc.observed
+    def likelihood(value = parts['binnedclusters'], logmassratios = parts['logmassratios'], c200s = parts['c200s']):
+
+        
 
 
 
