80b48cca853036069ff0248848b4fa6c2ab8a193
Douglas Applegate <dapple@astro.uni-bonn.de>
2014-12-12 16:19:45 +0100

    updated mxxlsnap41 answers to include m500

--- transform_mxxlanswers.py
+++ transform_mxxlanswers.py
@@ -7,6 +7,7 @@
 
 import sys, binaryutils, readtxtfile, cPickle, glob, os, re
 import astropy.io.ascii as asciireader
+import numpy as np
 
 ######################
 
@@ -43,19 +44,28 @@ if __name__ == '__main__':
 
     halocat = readtxtfile.readtxtfile('/users/dapple/astro/mxxlsims/mxxl_imperial/mxxlsnap%d/halos_%d.txt' % (snapnum, snapnum))
 
+    if snapnum == 41:
+        properties = readMXXLBinary('/users/dapple/astro/mxxlsims/mxxl_imperial/mxxlsnap41/Doug_XXL_z1.bin')
+        sort_props = np.argsort(properties['m200'])[::-1]
+
     clusterinfo = {}
 
     for halofile in glob.glob('/users/dapple/astro/mxxlsims/mxxl_imperial/mxxlsnap%d/*.convergence_map' % snapnum):
 
         halobase = os.path.basename(halofile)
         
-        match = re.match('halo_((\d+)_\d)\.convergence_map', halobase)
+        match = re.match('halo_%d_((\d+)_\d)\.convergence_map' % snapnum, 
+                         halobase)
         
         myid = match.group(1)
         stefan_id = int(match.group(2))
-        
 
-        clusterinfo[myid] = dict(m500 = 0.,
+        m500 = 0.
+        if snapnum == 41:
+            m500 = properties['M500_crit'][sort_props[stefan_id]]*1e10
+       
+
+        clusterinfo[myid] = dict(m500 = m500,
                                  m200 = halocat[stefan_id, 0]*1e10,
                                  concen = 0.,
                                  redshift = redshift)
