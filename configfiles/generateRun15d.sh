4e41e8942dd56c85d291b3c01f032c52a6431323
Douglas Applegate <douglas.e.applegate@gmail.com>
2015-05-14 16:19:20 +0200

    added rest of clusters for sz miscentering

---
+++ configfiles/generateRun15d.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -u
+
+
+sampling=0_0
+binning=hstnoisebins
+r=5
+
+for snap in 41 54; do
+
+    cat ../shearprofiles/coresizeindex.list | { while read cluster coresizeindex; do 
+
+	    for mc in duffy c4; do
+
+	    
+		config=hstnoisebins-${mc}-r${r}-core${coresizeindex}-${cluster}
+		dir=../../mxxl_lensing/mxxlsnap$snap/$config
+		if [ -e $dir ]; then
+		    mv $dir ../../mxxl_lensing/mxxlsnap$snap/old_${config}
+		fi
+		realdir=/vol/euclid1/euclid1_3/dapple/mxxl_lensing/mxxlsnap$snap/$config
+		mkdir $realdir
+		ln -s $realdir $dir
+		cat scanpdf.sh mxxl.sh ${mc}.sh r${r}.sh core_${coresizeindex}.sh ${binning}.sh > $realdir/config.sh
+
+		echo "profilefile=/vol/euclid1/euclid1_raid1/dapple/mxxlsims/shearprofiles/${cluster}.szcenter.profile" >> $realdir/config.sh
+	    
+		echo $config >> ../run15d.$snap
+
+
+
+	    
+
+
+	    done
+
+
+	done
+    }
+
+done
+
+
+	
