3e540052bdccd9ffafdeda1f5e6dceea8261b747
Douglas Applegate <dapple@astro.uni-bonn.de>
2015-05-15 14:15:30 +0200

    run15 generation

--- configfiles/generateRun15b.sh
+++ configfiles/generateRun15b.sh
@@ -17,7 +17,7 @@ for snap in 41 54; do
 		if [ -e $dir ]; then
 		    mv $dir ../../mxxl_lensing/mxxlsnap$snap/old_${config}
 		fi
-		realdir=/vol/euclid1/euclid1_3/dapple/mxxl_lensing/mxxlsnap$snap/$config
+		realdir=/vol/euclid1/euclid1_1/dapple/mxxl_lensing/mxxlsnap$snap/$config
 		mkdir $realdir
 		ln -s $realdir $dir
 		cat scanpdf.sh mxxl.sh ${mc}.sh r${r}.sh ${binning}.sh > $realdir/config.sh
--- configfiles/generateRun15c.sh
+++ configfiles/generateRun15c.sh
@@ -7,7 +7,7 @@ r=5
 
 for snap in 41 54; do
 
-    cat ../shearprofiles/reduced_coresizeindex.list | { while read cluster coresizeindex; do 
+    cat ../shearprofiles/coresizeindex.list | { while read cluster coresizeindex redshift; do 
 
 	    for mc in duffy c4; do
 
@@ -17,13 +17,13 @@ for snap in 41 54; do
 		if [ -e $dir ]; then
 		    mv $dir ../../mxxl_lensing/mxxlsnap$snap/old_${config}
 		fi
-		realdir=/vol/euclid1/euclid1_3/dapple/mxxl_lensing/mxxlsnap$snap/$config
+		realdir=/vol/euclid1/euclid1_1/dapple/mxxl_lensing/mxxlsnap$snap/$config
 		mkdir $realdir
 		ln -s $realdir $dir
 		cat scanpdf.sh mxxl.sh ${mc}.sh r${r}.sh core_${coresizeindex}.sh ${binning}.sh > $realdir/config.sh
 
 		echo "profilefile=/vol/euclid1/euclid1_raid1/dapple/mxxlsims/shearprofiles/${cluster}.szcenter.profile" >> $realdir/config.sh
-	    
+	    	echo "targetz=${redshift}" >> $realdir/config.sh
 		echo $config >> ../run15c.$snap
 
 
@@ -32,13 +32,14 @@ for snap in 41 54; do
 		if [ -e $dir ]; then
 		    mv $dir ../../mxxl_lensing/mxxlsnap$snap/old_${config}
 		fi
-		realdir=/vol/euclid1/euclid1_3/dapple/mxxl_lensing/mxxlsnap$snap/$config
+		realdir=/vol/euclid1/euclid1_1/dapple/mxxl_lensing/mxxlsnap$snap/$config
 		mkdir $realdir
 		ln -s $realdir $dir
 		cat scanpdf.sh mxxl.sh ${mc}.sh r${r}.sh ${binning}.sh > $realdir/config.sh
 
 		echo "profilefile=/vol/euclid1/euclid1_raid1/dapple/mxxlsims/shearprofiles/${cluster}.szcenter.profile" >> $realdir/config.sh
-		echo "sztheoreticalcentering=True" >> $dir/config.sh
+		echo "sztheoreticalcentering=True" >> $realdir/config.sh
+		echo "targetz=${redshift}" >> $realdir/config.sh
 	    
 		echo $config >> ../run15c.$snap
 
