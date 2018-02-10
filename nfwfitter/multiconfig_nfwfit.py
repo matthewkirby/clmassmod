f9522a3639fcdd4712a93088683f1abdc5894413
matthewkirby <matthewkirby@email.arizona.edu>
2018-02-09 23:33:00 -0700

    Began ini file building. Pausing work to check in

--- nfwfitter/multiconfig_nfwfit.py
+++ nfwfitter/multiconfig_nfwfit.py
@@ -4,53 +4,63 @@
 #  and runs multiple nfwfits, saving the results in different locations
 ######################
 
-import sys, json, os, shutil, glob
+import sys, os, shutil, glob
 import nfwfit
 import utils.simutils as simutils
+from configparser import ConfigParser
 
+
+'''
+Take the configuration object and kick off a run for each config file
+
+Parameters
+----------
+configparams : ConfigParser object
+               Contains all of the details about the runs the user wishes to use
+
+Returns
+-------
+None
+
+Notes
+-----
+MK 2/9: 
+    -Unsure what the doTransfer stuff is for. 
+    -Commenting this out and seeing if the code works without it.
+    -At some point down the line it can be deleted if a purpose is not discovered.
+'''
 
 def runMultiConfigs(jobparams, jobname=''):
 
-    inputfiles = jobparams['inputfiles']
     outputExt = jobparams['outputExt']
-    workbase = jobparams['workbase']
-    doTransfer = workbase is not None
-
-
-
-    inputname = jobparams['catname']
     outbasename = jobparams['outbasename']
-
-    if doTransfer:
-
-        inputbase = os.path.basename(inputname)
-
-        if jobname != '':
-            jobname = '-' + jobname
-
-        workdir = '{0}/{1}{2}'.format(workbase, inputbase, jobname)
-        print 'WORKDIR: ' + workdir
-
-
-        if not os.path.exists(workdir):
-            os.mkdir(workdir)
-
-        for inputfile in inputfiles:
-            shutil.copy(inputfile, workdir)
-
-
-        inputname = '{0}/{1}'.format(workdir, inputbase)
-
-        
-
     simreader = None
 
-
+    #inputfiles = jobparams['inputfiles']
+    #workbase = jobparams['workbase']
+    #doTransfer = workbase is not None
+    #if doTransfer:
+    #    inputbase = os.path.basename(inputname)
+    #
+    #    if jobname != '':
+    #        jobname = '-' + jobname
+    #
+    #    workdir = '{0}/{1}{2}'.format(workbase, inputbase, jobname)
+    #    print 'WORKDIR: ' + workdir
+    #
+    #
+    #    if not os.path.exists(workdir):
+    #        os.mkdir(workdir)
+    #
+    #    for inputfile in inputfiles:
+    #        shutil.copy(inputfile, workdir)
+    #
+    #
+    #    inputname = '{0}/{1}'.format(workdir, inputbase)
 
     for configfile in jobparams['configurations']:
 
         outdir = os.path.dirname(configfile)
-
         outputname = '{0}/{1}{2}'.format(outdir, outbasename, outputExt)
 
         print configfile, outputname
@@ -73,27 +83,34 @@ def runMultiConfigs(jobparams, jobname=''):
         
         shutil.rmtree(workdir)
 
-########
 
-def loadJobfile(jobfile):
-
-    with open(jobfile) as input:
-        jobparams = json.load(input)
-
-    return jobparams
     
-#########
+    
+'''
+Run the NFW fit for each set of configurations
 
-def runAll(jobfile):
+The user is permitted to pass in any amount of configurations. NFWfit will then be run on each
+set of configurations.
 
-    jobparams = loadJobfile(jobfile)
-    runMultiConfigs(jobparams)
+Parameters
+----------
+configfile : string
+             Name of the config file
+             
+Returns
+-------
+None
+'''
 
-#########
+def runAll(configfile):
+    configparams = ConfigParser()
+    configparams.read(configfile)
+    runMultiConfigs(configparams)
+
 
 if __name__ == '__main__':
 
-    jobfile = sys.argv[1]
+    configfile = sys.argv[1]
 
-    runAll(jobfile)
+    runAll(configfile)
 
